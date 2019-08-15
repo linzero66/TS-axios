78e4811b88197e0d5b368d55f46044719ce7c6f4
linzero <806307567@qq.com>
2019-08-15 12:00:26 +0800

    add

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,16 +7,16 @@ export interface AxiosRequestConfig {
   params?: any
   headers?: any
   responseType?: XMLHttpRequestResponseType
-  timeout?:number
+  timeout?: number
 }
 
 export interface AxiosResponse {
-  data: any //服务端返回的数据
-  status: number //HTTP 状态码
-  statusText: string //状态消息
-  headers: any  //响应头
-  config: AxiosRequestConfig //请求配置对象
-  request: any //请求的 XMLHttpRequest 对象实例
+  data: any // 服务端返回的数据
+  status: number // HTTP 状态码
+  statusText: string // 状态消息
+  headers: any  // 响应头
+  config: AxiosRequestConfig // 请求配置对象
+  request: any // 请求的 XMLHttpRequest 对象实例
 }
 
 export type Method = 'get' | 'GET'
@@ -30,10 +30,10 @@ export type Method = 'get' | 'GET'
 export interface AxiosPromise extends Promise<AxiosResponse> {
 }
 
-export interface AxiosError extends Error{
-    config:AxiosRequestConfig
-    code?:string
-    request?:any
-    response?:AxiosResponse
-    isAxiosError:boolean
+export interface AxiosError extends Error {
+  config: AxiosRequestConfig
+  code?: string
+  request?: any
+  response?: AxiosResponse
+  isAxiosError: boolean
 }
